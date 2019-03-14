d6c89d5c7e00a4847c868435896d917b7abe5ef5
JYOTHI <JYOTHI@JYOTHI-PC>
2019-03-14 17:19:04 +0530

    Commit

---
+++ testProject/src/com/testProject/lering/AddFuncton.java
@@ -0,0 +1,15 @@
+package com.testProject.lering;
+
+public class AddFuncton {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=10,b=36,c;
+		c=a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
